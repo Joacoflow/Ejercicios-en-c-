018c9f60a36d8264e94cc692608dc831b86e7206
Joacoflow <joaquinalfredo6@gmail.com>
2023-04-09 06:28:24 -0500

    Se Agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] edades = new int[4, 4];
+            int numero = 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    edades[i, j] = numero;
+                    numero = numero + 1;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(edades[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
